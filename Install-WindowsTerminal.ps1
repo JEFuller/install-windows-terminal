c2c85458890ec6a2113d0c90843e6f121bcb20a3
ahpooch <81159041+ahpooch@users.noreply.github.com>
2025-01-25 17:54:43 +0300

    Update Install-WindowsTerminal.ps1

--- Install-WindowsTerminal.ps1
+++ Install-WindowsTerminal.ps1
@@ -8,14 +8,23 @@ if ($null -ne $Package) {
 Push-Location $Env:TEMP
 
 Write-Host "Downloading Dependencies..."
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x86.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.5/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/terminal/releases/download/v1.19.11213.0/Microsoft.WindowsTerminal_1.19.11213.0_8wekyb3d8bbwe.msixbundle -OutFile Microsoft.WindowsTerminal_1.19.11213.0_8wekyb3d8bbwe.msixbundle 
+### Download Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+
+### Download Microsoft.UI.Xaml.2.8.x64.appx
+#Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.5/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+
+### Download Windows Terminal
+# Windows Terminal Latest 1.21.3231.0
+#Invoke-WebRequest -Uri https://github.com/microsoft/terminal/releases/download/v1.21.3231.0/Microsoft.WindowsTerminal_1.21.3231.0_8wekyb3d8bbwe.msixbundle -OutFile Microsoft.WindowsTerminal_1.21.3231.0_8wekyb3d8bbwe.msixbundle
+# Windows Terminal Preview Pre-Release 1.22.3232.0
+Invoke-WebRequest -Uri https://github.com/microsoft/terminal/releases/download/v1.22.3232.0/Microsoft.WindowsTerminalPreview_1.22.3232.0_8wekyb3d8bbwe.msixbundle -OutFile Microsoft.WindowsTerminalPreview_1.22.3232.0_8wekyb3d8bbwe.msixbundle
 
 Write-Host "Installing Dependencies..."
-Add-AppxPackage Microsoft.VCLibs.x86.14.00.Desktop.appx
+Add-AppxPackage .\Microsoft.VCLibs.x64.14.00.Desktop.appx
 Add-AppxPackage .\Microsoft.UI.Xaml.2.8.x64.appx
-Add-AppxPackage .\Microsoft.WindowsTerminal_1.19.11213.0_8wekyb3d8bbwe.msixbundle
+Add-AppxPackage .\Microsoft.WindowsTerminalPreview_1.22.3232.0_8wekyb3d8bbwe.msixbundle
 
 Write-Host "Installed Microsoft.WindowsTerminal."
 Pop-Location
