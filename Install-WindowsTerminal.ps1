0ea9c656bd55aae89b773a1caeed62df46b6ff7f
ahpooch <81159041+ahpooch@users.noreply.github.com>
2025-01-26 00:31:56 +0300

    Update Install-WindowsTerminal.ps1

--- Install-WindowsTerminal.ps1
+++ Install-WindowsTerminal.ps1
@@ -5,27 +5,22 @@ if ($null -ne $Package) {
     exit
 }
 
-Push-Location $Env:TEMP
-
-Write-Host "Downloading Dependencies..."
 ### Download Microsoft.VCLibs.x64.14.00.Desktop.appx
-Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile "$env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx"
 
 ### Download Microsoft.UI.Xaml.2.8.x64.appx
-#Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.5/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
-Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile "$env:temp\Microsoft.UI.Xaml.2.8.x64.appx"
 
 ### Download Windows Terminal
 # Windows Terminal Latest 1.21.3231.0
-#Invoke-WebRequest -Uri https://github.com/microsoft/terminal/releases/download/v1.21.3231.0/Microsoft.WindowsTerminal_1.21.3231.0_8wekyb3d8bbwe.msixbundle -OutFile Microsoft.WindowsTerminal_1.21.3231.0_8wekyb3d8bbwe.msixbundle
-# Windows Terminal Preview Pre-Release 1.22.3232.0
-Invoke-WebRequest -Uri https://github.com/microsoft/terminal/releases/download/v1.22.3232.0/Microsoft.WindowsTerminalPreview_1.22.3232.0_8wekyb3d8bbwe.msixbundle -OutFile Microsoft.WindowsTerminalPreview_1.22.3232.0_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://github.com/microsoft/terminal/releases/download/v1.21.3231.0/Microsoft.WindowsTerminal_1.21.3231.0_8wekyb3d8bbwe.msixbundle -OutFile "$env:temp\Microsoft.WindowsTerminal_1.21.3231.0_8wekyb3d8bbwe.msixbundle"
 
-Write-Host "Installing Dependencies..."
-Add-AppxPackage .\Microsoft.VCLibs.x64.14.00.Desktop.appx
-Add-AppxPackage .\Microsoft.UI.Xaml.2.8.x64.appx
-Add-AppxPackage .\Microsoft.WindowsTerminalPreview_1.22.3232.0_8wekyb3d8bbwe.msixbundle
+#Add-AppxPackage .\Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-ProvisionedAppPackage -Online -PackagePath "$env:temp\Microsoft.VCLibs.x64.14.00.Desktop.appx" -SkipLicense
+#Add-AppxPackage .\Microsoft.UI.Xaml.2.8.x64.appx
+Add-ProvisionedAppPackage -Online -PackagePath "$env:temp\Microsoft.UI.Xaml.2.8.x64.appx" -SkipLicense
+#Add-AppxPackage .\Microsoft.WindowsTerminalPreview_1.22.3232.0_8wekyb3d8bbwe.msixbundle
+Add-ProvisionedAppPackage -Online -PackagePath "$env:temp\Microsoft.WindowsTerminalPreview_1.22.3232.0_8wekyb3d8bbwe.msixbundle" -SkipLicense
 
-Write-Host "Installed Microsoft.WindowsTerminal."
-Pop-Location
+Write-Host "Microsoft.WindowsTerminal installed successfull."
 Pause
